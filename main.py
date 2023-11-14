fab4da495cbc6948b3ac57402759e39c934c5870
Davide Mattavelli <Davide.mattavelli@beta80group.it>
2023-11-14 15:08:13 +0100

    aggiunta-credenziali-completata

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
+#!/usr/bin/env python3
 import yaml
 
-def aggiungi_credenziale(path, newCred):
+
+def aggiungi_credenziale(path, newCred, new_id):
     with open(path, "r") as file:
         dati: list
         dati = yaml.safe_load(file)
-        print (dati)
-        dati["credenziali"].append(newCred)
-
+        dati.append(newCred)
         with open(path, "w") as file:
             yaml.dump(dati, file)
 
+
+def assegnaId(path):
+    with open(path, "r") as file:
+        dati: list
+        dati = yaml.safe_load(file)
+        newid:int
+        newid = dati[len(dati)-1]["id"] + 1
+        return newid
+# Main
+
+
 def main():
-    file_path = "data.yml"
-    nuova_credenziale = {
-        "username"
-    }
-    aggiungi_credenziale()
-# new_id
-# new_label = input("scrivi un nuovo label")
-# new_username = input("scrivi un nuovo username")
-# new_password = input("scrivi una nuova password")
-# new_note = input("scrivi nuove note")
-# path = "data.yml"
-# newCred = {
-#     "id" : ""
-#     "label" : "new_label"
-#     "username": "new_username"
-#     "password": "new_password"
-#     "note": "new_note"
-# }
+    pass
+
+
+
+# Execute
+
+if __name__ == '__main__':
+
+    main()
+
+
