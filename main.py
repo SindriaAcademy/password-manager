8e84089e38a679499457e9ee9af0b86c1173ee74
Samuele Cesaroni <samuele.cesaroni@samuele.cesaroni.com>
2023-11-14 16:26:52 +0100

    Modificato

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import yaml
-# Costanti Globali
 
+# Costanti Globali
 CREDENTIALS_FILE_PATH = "app.yml"
 MAX_LOGIN_ATTEMPTS = 3
 
@@ -25,50 +25,62 @@ def login(username, password, credentials):
         if stored_username == username.strip() and stored_password == password.strip():
             return True
     return False
-def main():
-    pass
+
+
+def perform_login():
     credentials = load_credentials(CREDENTIALS_FILE_PATH)
     is_logged_in = False
+    attempts = 0
 
-    while not is_logged_in:
-        for attempt in range(MAX_LOGIN_ATTEMPTS):
-            print(f"Attempt {attempt + 1}/{MAX_LOGIN_ATTEMPTS}")
-            username = input("Inserisci username: ")
-            password = input("Inserisci password: ")
+    while not is_logged_in and attempts < MAX_LOGIN_ATTEMPTS:
+        print(f"Attempt {attempts + 1}/{MAX_LOGIN_ATTEMPTS}")
+        username = input("Inserisci username: ")
+        password = input("Inserisci password: ")
 
-            if login(username, password, credentials):
-                full_name = next((user_info.get("fullname", "") for user_info in credentials if user_info.get("username") == username), "")
-                print(f"Login avvenuto, Ciao {full_name}!")
-                is_logged_in = True
-                break
-            else:
-                print("Login non effettuato, riprova")
-
+        if login(username, password, credentials):
+            full_name = next((user_info.get("fullname", "") for user_info in credentials if user_info.get("username") == username), "")
+            print(f"Login avvenuto, Ciao {full_name}!")
+            is_logged_in = True
         else:
-            print("Raggiunti tentativi massimi, Riprova piu tardi coglione.")
+            print("Login non effettuato, riprova")
+            attempts += 1
 
-            sys.exit()
+    if not is_logged_in:
+        print("Raggiunti tentativi massimi, Riprova piu tardi coglione.")
+        sys.exit()
 
-        while is_logged_in:
-            print("\nLista delle funzioni CRUD di credenziali:")
-            print("1. Visualizza credenziali")
-            print("2. Aggiungi nuova credenziale")
-            print("3. Modifica credenziale esistente")
-            print("4. Elimina credenziale")
-            print("5. Esci")
+    return credentials,  is_logged_in
 
-            choice = input("Scegli un'opzione (1-5): ")
-            if choice == "1":
-                print("Visualizza credenziali: Implementazione di visualizzazione")
-            elif choice == "2":
-                print("Aggiungi nuova credenziale: Implementazione di aggiunta")
-            elif choice == "3":
-                print("Modifica credenziale esistente: Implementazione di modifica")
-            elif choice == "4":
-                print("Elimina credenziale: Implementazione di eliminazione")
-            elif choice == "5":
-                print("Uscita dal programma.")
-                is_logged_in = False
+
+def menu():
+    print("\nLista delle funzioni CRUD di credenziali:")
+    print("1. Visualizza credenziali")
+    print("2. Aggiungi nuova credenziale")
+    print("3. Modifica credenziale esistente")
+    print("4. Elimina credenziale")
+    print("5. Esci")
+
+    choice = input("Scegli un'opzione (1-5): ")
+
+    if choice == "1":
+        print("Visualizza credenziali: view_all_credentials()")
+    elif choice == "2":
+        print("Aggiungi nuova credenziale:  aggiungi_credenziale()")
+    elif choice == "3":
+        print("Modifica credenziale esistente: edit_credential()")
+    elif choice == "4":
+        print("Elimina credenziale: Implementazione di eliminazione")
+    elif choice == "5":
+        print("Uscita dal programma.")
+        sys.exit()
+
+    return True
+
+def main():
+    credentials, is_logged_in = perform_login()
+
+    while is_logged_in:
+        is_logged_in = menu()
 
 if __name__ == '__main__':
     main()
