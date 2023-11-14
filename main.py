5af451296c13577c03d7a68f33e617df80b30879
Samuele Cesaroni <samuele.cesaroni@samuele.cesaroni.com>
2023-11-14 12:50:38 +0100

    Aggiunto Exit() e variabili globali

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import sys
 import yaml
+# Definisci costanti globali per i percorsi
+CREDENTIALS_FILE_PATH = "app.yml"
+MAX_LOGIN_ATTEMPTS = 3
 
 def load_credentials(filename):
     with open(filename, "r") as file:
@@ -15,53 +19,49 @@ def login(username, password, credentials):
     return False
 
 def main():
-    max_attempts = 3
-    filename = "app.yml"  # Cambia questo al nome corretto del tuo file YAML
-    credentials = load_credentials(filename)
-    is_logged_in = False  # Aggiunta della variabile di stato del login
+    # Usa le costanti globali per i percorsi
+    credentials = load_credentials(CREDENTIALS_FILE_PATH)
+    is_logged_in = False
 
     while not is_logged_in:
-        for attempt in range(max_attempts):
-            print(f"Attempt {attempt + 1}/{max_attempts}")
+        for attempt in range(MAX_LOGIN_ATTEMPTS):
+            print(f"Attempt {attempt + 1}/{MAX_LOGIN_ATTEMPTS}")
             username = input("Inserisci username: ")
             password = input("Inserisci password: ")
 
             if login(username, password, credentials):
                 full_name = next((user_info.get("fullname", "") for user_info in credentials if user_info.get("username") == username), "")
                 print(f"Login avvenuto, Ciao {full_name}!")
-                is_logged_in = True  # Imposta la variabile di stato del login a True
+                is_logged_in = True
                 break
             else:
                 print("Login non effettuato, riprova")
 
         else:
-            print("Raggiunti tentativi massimi, riprovare tra un po'")
+            print("Raggiunti tentativi massimi, Riprova piu tardi coglione.")
 
-    # Se l'utente è loggato, mostra le opzioni CRUD
-    while is_logged_in:
-        print("\nLista delle funzioni CRUD di credenziali:")
-        print("1. Visualizza credenziali")
-        print("2. Aggiungi nuova credenziale")
-        print("3. Modifica credenziale esistente")
-        print("4. Elimina credenziale")
-        print("5. Esci")
+            sys.exit()
 
-        choice = input("Scegli un'opzione (1-5): ")
-        if choice == "1":
-            print("Visualizza credenziali: Implementazione di visualizzazione")
-        elif choice == "2":
-            print("Aggiungi nuova credenziale: Implementazione di aggiunta")
-        elif choice == "3":
-            print("Modifica credenziale esistente: Implementazione di modifica")
-        elif choice == "4":
-            print("Elimina credenziale: Implementazione di eliminazione")
-        elif choice == "5":
-            print("Uscita dal programma.")
-            is_logged_in = False  # Imposta la variabile di stato del login a False per uscire dalla sessione
-        else:
-            print("Scelta non valida. Riprova.")
+        while is_logged_in:
+            print("\nLista delle funzioni CRUD di credenziali:")
+            print("1. Visualizza credenziali")
+            print("2. Aggiungi nuova credenziale")
+            print("3. Modifica credenziale esistente")
+            print("4. Elimina credenziale")
+            print("5. Esci")
 
-
+            choice = input("Scegli un'opzione (1-5): ")
+            if choice == "1":
+                print("Visualizza credenziali: Implementazione di visualizzazione")
+            elif choice == "2":
+                print("Aggiungi nuova credenziale: Implementazione di aggiunta")
+            elif choice == "3":
+                print("Modifica credenziale esistente: Implementazione di modifica")
+            elif choice == "4":
+                print("Elimina credenziale: Implementazione di eliminazione")
+            elif choice == "5":
+                print("Uscita dal programma.")
+                is_logged_in = False
 
 if __name__ == "__main__":
     main()
